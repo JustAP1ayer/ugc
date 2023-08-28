a471503c44a54a5b8246276989a6064dcfce60c7
JustAP1ayer <70037178+JustAP1ayer@users.noreply.github.com>
2023-08-27 21:44:29 -0400

    Create rnggamenoautoexec.lua

---
+++ rnggamenoautoexec.lua
@@ -0,0 +1,62 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+repeat
+    wait(0.1)
+until game.Loaded and game.Players.LocalPlayer.Character and
+    game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+
+task.wait(0.15)
+if _G.MinTimer == nil then
+    _G.MinTimer = 30
+end
+      queue_on_teleport([[
+     if not game:IsLoaded() then game.Loaded:Wait() end
+  repeat wait(0.1) until  game.Loaded and game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart") 
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/JustAP1ayer/ugc/main/rnggamenoautoexec.lua"),true))()
+    ]])
+
+if game.ReplicatedStorage.nextSpin.Value <= _G.MinTimer then
+
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "AFK, Waiting " .. tostring(game.ReplicatedStorage.nextSpin.Value) .. "s",
+        Duration = game.ReplicatedStorage.nextSpin.Value - 2,
+        Text = "Waiting for reward"
+    })
+
+    repeat
+        task.wait()
+    until tonumber(game.ReplicatedStorage.nextSpin.Value) > _G.MinTimer
+    task.wait(2)
+    if game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator:FindFirstChild("Prompt") then
+        task.wait(1)
+
+        repeat
+            wait(0.1)
+        until not game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator:FindFirstChild("Prompt")
+        task.wait(2)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
+    elseif not game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator:FindFirstChild("Prompt") then
+
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
+    end
+else
+
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "redblue on top",
+        Duration = 3,
+        Text = "Serverhopping (maybe)"
+    })
+    task.wait(2.5)
+    if game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator:FindFirstChild("Prompt") then
+        task.wait(1)
+
+        repeat
+            wait(0.1)
+        until not game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator:FindFirstChild("Prompt")
+        task.wait(2)
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
+    elseif not game.CoreGui.PurchasePrompt.ProductPurchaseContainer.Animator:FindFirstChild("Prompt") then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/JustAP1ayer/ignorewtffxd/main/serverhopxd.lua", true))()
+    end
+end
